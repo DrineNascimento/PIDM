17a0b1ec20dbcc7491f0a79d4d779f0cab9db2d1
Andrine Nascimento <andrinenascimento.7@gmail.com>
2021-10-03 11:17:05 -0300

    primeiro commit

---
+++ components/OlaMundo.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import {Text, View} from 'react-native'
+
+export default class OlaMundo extends Component{
+    render(){
+        //aqui dentro fica o codigo JSX
+        return(
+            <View>
+                <Text>
+                    Primeira aula de React
+                </Text>
+            </View>
+        )
+    }
+}
---
+++ components/aluno.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Text, View} from 'react-native'
+
+export default function Aluno(){
+        return(
+            <View>
+                <Text style= {{color:'red', fontWeight:'bold'}}>
+                    Andrine, 23 anos, Sobral, Design Digital
+                </Text>
+            </View>
+        )
+}
